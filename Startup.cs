f9811ef791b3098eca9f8e5b2d3d5a299dc72fcd
stewartgemmill-hub <stewart.gemmill@googlemail.com>
2026-08-10 14:33:40 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TutionCloudWeb.Startup))]
+namespace TutionCloudWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
